b16cba6660e202cf5578ed39f73e5f78c108917c
Alexey <alex65-2@mail.ru>
2023-11-08 10:48:30 +0300

    update directory

--- fastapi_app/frontend/front_api.py
+++ fastapi_app/frontend/front_api.py
@@ -1,37 +1,22 @@
-import asyncio
-import json
-import uvicorn
-
-from fastapi import FastAPI, Request
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from tortoise import Tortoise
 
-from message_model import MessageModel
-
-
-async def init_tortoise():
-    with open("db_config.json") as f:
-        json_data = json.load(f)
-
-    await Tortoise.init(
-        config=json_data,
-        use_tz=True
-    )
-    await Tortoise.generate_schemas(safe=True)
+from fastapi_app.message_model import MessageModel
 
 
-app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+router = APIRouter()
+
+router.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/messages", response_class=HTMLResponse)
+@router.get("/messages", response_class=HTMLResponse)
 async def read_item(request: Request):
     records_message: list[MessageModel] = await MessageModel.all()
     dialog_list = set()
@@ -42,19 +27,9 @@ async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "dialog_list": dialog_list})
 
 
-@app.get("/dialog/{chat_id}", response_class=HTMLResponse)
+@router.get("/dialog/{chat_id}", response_class=HTMLResponse)
 async def read_item(request: Request, chat_id):
     records_message: list[MessageModel] = await MessageModel.filter(chat_id=chat_id).all()
     print(records_message)
     return templates.TemplateResponse("chat.html", {"request": request, "records_message": records_message})
 
-
-
-async def main():
-    await init_tortoise()
-    config = uvicorn.Config("app:app", port=8080)
-    server = uvicorn.Server(config)
-    await server.serve()
-
-if __name__ == "__main__":
-    asyncio.run(main())
